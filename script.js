c3a759cbaafdd6956c71e9a76ece6617d55fadaf
Florencia Rodriguez <148459961+FlorR566@users.noreply.github.com>
2025-05-26 11:17:33 -0300

    Add files via upload
    Se acomodan archivos y actualiza el fondo.

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-  if(inputBox.value === ''){
-    alert("You must write something!");
-  } 
-  else{
-    let li= document.createElement("li");
-    li.innerHTML = inputBox.value;
-    listContainer.appendChild(li);
-    let span = document.createElement("span");
-    span.innerHTML= "\u00d7";
-    li.appendChild(span);
-  }
-  inputBox.value = ""; 
-  saveData();
+function addTask() {
+	if (inputBox.value === "") {
+		alert("You must write something!");
+	} else {
+		let li = document.createElement("li");
+		li.innerHTML = inputBox.value;
+		listContainer.appendChild(li);
+		let span = document.createElement("span");
+		span.innerHTML = "\u00d7";
+		li.appendChild(span);
+	}
+	inputBox.value = "";
+	saveData();
 }
 
-listContainer. addEventListener("click", function(e){
-  if(e.target.tagName === "LI"){
-      e.target.classList.toggle("checked"); //este código es para marcar check o descheck
-      saveData();
-}
-  else if(e.target. tagName === "SPAN" ){
-      e.target.parentElement.remove(); //este código es para eliminar las tareas
-      saveData();
-    }
-  },false);
+listContainer.addEventListener(
+	"click",
+	function (e) {
+		if (e.target.tagName === "LI") {
+			e.target.classList.toggle("checked"); //este código es para marcar check o descheck
+			saveData();
+		} else if (e.target.tagName === "SPAN") {
+			e.target.parentElement.remove(); //este código es para eliminar las tareas
+			saveData();
+		}
+	},
+	false
+);
 
-  function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-  }
-  function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-  }
-  showTask();
+function saveData() {
+	localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+	listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
